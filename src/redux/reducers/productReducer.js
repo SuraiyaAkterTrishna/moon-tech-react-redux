9584058f4098f54e4be38e69675c9b45a3c98782
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-12-16 02:32:05 +0600

    add to cart state

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,9 +1,26 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/productActionTypes";
+
 const initialState = {
-    test : 'test'
+    cart : [],
 };
 
 const productReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case ADD_TO_CART:
+            return{
+                ...state,
+                cart : [...state.cart, action.payload]
+            };
+
+        case REMOVE_FROM_CART:
+            return{
+                ...state,
+                cart : [...state.cart, action.payload]
+            };
+    
+        default:
+            return state;
+    }
 };
 
 export default productReducer;
