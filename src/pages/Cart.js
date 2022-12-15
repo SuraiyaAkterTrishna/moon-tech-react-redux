32a62abc66bad514f2bc7b94ab7df1c5387677f2
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-12-16 01:18:56 +0600

    first commit

---
+++ src/pages/Cart.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+
+const Cart = () => {
+  // const {state : {cart, loading, error}} = useProduct();
+  let content;
+  // if(loading){
+  //   content = <p>Loading............................Wait Few Moment</p>
+  // };
+  // if(error){
+  //   content = <p>Danger............................Something went wrong</p>
+  // };
+  // if(!loading && !error && cart.length === 0){
+  //   content = <p>Nothing to show............................cart is Empty</p>
+  // };
+  // if(!loading && !error && cart.length){
+  //   content = cart.map(product => <ProductCard key={product.id} product={product} />)
+  // };
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+      {content}
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/TopRated.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ProductCard from '../components/ProductCard';
+
+const TopRated = () => {
+  // const {state : {products, loading, error}} = useProduct();
+  let content;
+  // if(loading){
+  //   content = <p>Loading............................Wait Few Moment</p>
+  // };
+  // if(error){
+  //   content = <p>Danger............................Something went wrong</p>
+  // };
+  // if(!loading && !error && products.length === 0){
+  //   content = <p>Nothing to show............................Products is Empty</p>
+  // };
+  // if(!loading && !error && products.length){
+  //   content = products.filter(product => product.rating >= 4).map(product => <ProductCard key={product.id} product={product} />)
+  // };
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+      {content}
+    </div>
+  );
+};
+
+export default TopRated;
